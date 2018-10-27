9ba85fde3a63952698dbe9b5df4577600d4fb7ca
KALIDOU-GATTA-BA <40792787+KALIDOU-GATTA-BA@users.noreply.github.com>
2018-10-27 12:00:24 +0200

    file move to /view/frontend

--- controller/contact1.php
+++ controller/contact1.php
@@ -1,13 +1,3 @@
 <?php
 
-          require_once("../model/MailsManager.php");
-          $req=new MailsManager();
-          $req=$req->emails();
-          $message=$req['messages'];
-          $email=$req['emails'];
-  
-          mail( 'kalidougattaba@gmail.com', 'Nouveau message', 'Bonjour Jean, ' .$email. ' vous a envoyé ce message: ' .$message );
-
-          echo"Votre message a bien été envoyé "; ?>
-
-         <br> <a href="../index.php">Retour</a> 
+header("location:../view/frontend/contactView.php");
