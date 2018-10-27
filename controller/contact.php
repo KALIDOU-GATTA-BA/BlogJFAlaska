5f0c328cf6fe46ae62ad172b6e59660642e65c4f
KALIDOU-GATTA-BA <40792787+KALIDOU-GATTA-BA@users.noreply.github.com>
2018-10-27 11:57:04 +0200

    Update contact.php

--- controller/contact.php
+++ controller/contact.php
@@ -1,27 +1,13 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link href="../public/css/style.css" rel="stylesheet" />
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-<body>
-<div class="container">
-  <form action="contact1.php" method="post">
-    <div class="form-group">
-      <label for="email">Email:</label>
-      <input type="email" class="form-control" id="email" placeholder="Entrez votre email" name="email" required>
-    </div>
-    <div class="form-group">
-      <label for="pwd">Message:</label><br>
-     <textarea  style="width:100%; height:120px" name="message"> </textarea>
-    </div>
-    <input type="submit"  value="Envoyer" class="btn btn-default">
-  </form> <br>
- <a href="../index.php"><span class="glyphicon glyphicon-remove"> </span> <strong>Annuler</strong></a>
-</div>
-</body>
-</html>
+<?php
+ 
+          require_once("../model/MailsManager.php");
+          $req=new MailsManager();
+          $req=$req->emails();
+          $message=$req['messages'];
+          $email=$req['emails'];
+ 
+          mail( 'kalidougattaba@gmail.com', 'Nouveau message', 'Bonjour Jean, ' .$email. ' vous a envoyé ce message: ' .$message );
+ 
+          echo"Votre message a bien été envoyé "; ?>
+ 
+         <br> <a href="../index.php">Retour</a> 
