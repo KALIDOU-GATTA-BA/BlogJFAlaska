a8e7ef5f47d0257d9aeec47c2ff80df5befa0ba3
KALIDOU-GATTA-BA <40792787+KALIDOU-GATTA-BA@users.noreply.github.com>
2018-10-27 11:43:09 +0200

    Update contact1.php

--- controller/contact1.php
+++ controller/contact1.php
@@ -1,22 +1,13 @@
 <?php
-echo"Votre message a bien été envoyé "; ?>
-<br> <a href="../index.php">Retour</a>
 
-<?php
-require_once("../model/Manager.php");
-$dbAcess=new Manager;
-$bdd=$dbAcess->dbConnect();
-
-$req = $bdd->prepare("INSERT INTO user( email, message, username, password) VALUES(?, ?, '', '')");
-$req = $req->execute(array(htmlspecialchars($_POST['email']), htmlspecialchars($_POST['message'])));
+          require_once("../model/MailsManager.php");
+          $req=new MailsManager();
+          $req=$req->emails();
+          $message=$req['messages'];
+          $email=$req['emails'];
   
-$req=$bdd->query("SELECT email, message from user order by id desc limit 0,1 ");
-$req = $req->fetch();
-$message=$req['message'];
-$email=$req['email'];
- 
-mail( 'kalidougattaba@gmail.com', 'Nouveau message', 'Bonjour Jean, ' .$email. ' vous a envoyé ce message: ' .$message );
-/*$req=$bdd->query("SELECT max(id) as maxId from user ");
-$req = $req->fetch();
-$idM=$req['maxId'];
-$req=$bdd->query("DELETE * FROM user where id=$idM ");
+          mail( 'kalidougattaba@gmail.com', 'Nouveau message', 'Bonjour Jean, ' .$email. ' vous a envoyé ce message: ' .$message );
+
+          echo"Votre message a bien été envoyé "; ?>
+
+         <br> <a href="../index.php">Retour</a> 
