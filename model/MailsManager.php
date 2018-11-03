782cdc01bf7d417b82440632554919153fa7da86
KALIDOU-GATTA-BA <40792787+KALIDOU-GATTA-BA@users.noreply.github.com>
2018-11-03 14:27:54 +0100

    Emails check

--- model/MailsManager.php
+++ model/MailsManager.php
@@ -5,17 +5,20 @@ include $Manager . '.php';
 });
  
 class MailsManager extends Manager
- 
+
 {
 	public function emails()
 	{ 
+               if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+              
 		$db=$this->dbConnect();
- 
+		 
 		$req = $db->prepare("INSERT INTO contacts( emails, messages) VALUES(?, ?)");
 		$req = $req->execute(array(htmlspecialchars($_POST['email']), htmlspecialchars($_POST['message'])));
- 
+		 
 		$req=$db->query("SELECT emails, messages from contacts order by id desc limit 0,1 ");
 		$req = $req->fetch();
 		return $req;
+             }
 	}
 }
