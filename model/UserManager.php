31a7ec878e9b8bfd72fe02fe349cacafb6530596
KALIDOU-GATTA-BA <40792787+KALIDOU-GATTA-BA@users.noreply.github.com>
2018-11-06 20:37:21 +0100

    Update UserManager.php

--- model/UserManager.php
+++ model/UserManager.php
@@ -1,27 +1,13 @@
 <?php
 require_once("Manager.php");
-
+ 
 class UserManager extends Manager
 {
-      public function verification(){
-
+          public function verification(){
+ 
           $db=$this->dbConnect();
           $req=$db->query('SELECT * FROM user');
           $data=$req->fetch();
-          $usn=$data['username'];
-          $hash=$data['password'];
-         if (password_verify($_POST['password'], $hash)&&($_POST['username'] === $usn))
-                { 
-                
-                $_SESSION['username'] = $username;
-                header('Location:../view/backend/indexBackend.php');
-           }       
-        else
-            {
-           echo"Utilisateur ou mot de passe incorrect !"; ?>
-            <a href="login.php">Réessayer</a> <?php
-        } 
-
-      }
-
+          return $data;   
+     }
 }
