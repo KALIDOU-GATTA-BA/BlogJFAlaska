c194287b8a2e17ccf93c71a94686669f5f667786
KALIDOU-GATTA-BA <40792787+KALIDOU-GATTA-BA@users.noreply.github.com>
2018-11-06 20:49:56 +0100

    session check

--- controller/verification.php
+++ controller/verification.php
@@ -1,8 +1,14 @@
 <?php
-    session_start();
+ 
     require_once("../model/UserManager.php");
-   
+ 
     $req=new UserManager();
     $req=$req-> verification();
-   
- 
+    if(isset ($_SESSION["admin"]) &&  ($_SESSION["admin"]=="admin")){
+                    $fichier = '../view/backend/.htaccess';
+                    @unlink( $fichier ) ;
+                    header('Location:../view/backend/indexBackend.php');    
+    }
+    else{
+                    echo"Identifiant ou mot de passe incorrect, veuillez" ; ?> <a href="login.php"> réessayer </a>
+<?php } ?>
