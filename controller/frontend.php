46f126985f1948f057c5e18b05497cc8e7fcb5af
KALIDOU-GATTA-BA <40792787+KALIDOU-GATTA-BA@users.noreply.github.com>
2018-11-06 20:46:35 +0100

    Update frontend.php

--- controller/frontend.php
+++ controller/frontend.php
@@ -1,5 +1,5 @@
 <?php
-
+ 
 require_once('model/PostManager.php');
 require_once('model/CommentManager.php');
 require_once("model/Manager.php");
@@ -7,37 +7,38 @@ function listPosts()
 {
     $postManager = new PostManager(); 
     $posts = $postManager->getPosts(); 
-
+ 
     require('view/frontend/listPostsView.php');
 }
-
+ 
 function post()
 {
     $postManager = new PostManager();
-    $commentManager = new CommentManager();
-
     $post = $postManager->getPost($_GET['id']);
+    $title =$post['title']; 
+    $content=nl2br($post['content']);
+    $post=$post['creation_date_fr'];  
+    $commentManager = new CommentManager();    
     $comments = $commentManager->getComments($_GET['id']);
-
-    require('view/frontend/postView.php');
+    require('view/frontend/postView.php');  
 }
-
+ 
 function addComment($postId, $author, $comment)
 {
-
+ 
     $commentManager = new CommentManager();
-
+ 
     $affectedLines =$commentManager->postComment($postId, $author, $comment);
-
+ 
     if ($affectedLines === false) {
         throw new Exception('Impossible d\'ajouter le commentaire !');
     }
     else {      
-             
+ 
             $req= new CommentManager();
             $req=$req->countComment($postId);
             mail( 'kalidougattaba@gmail.com', 'Nouveau Commentaire', 'Bonjour Jean, quelqu\'un vient de commenter un des chapitres de votre blog' );
             header('Location: index.php?action=post&id=' . $postId);
     }
-    
+ 
 }
