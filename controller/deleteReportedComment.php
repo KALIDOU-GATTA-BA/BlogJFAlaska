f7d16e5a97d54a9a7cdf6b732265e0e003e2b382
KALIDOU-GATTA-BA <40792787+KALIDOU-GATTA-BA@users.noreply.github.com>
2018-11-06 20:59:41 +0100

    Update deleteReportedComment.php

--- controller/deleteReportedComment.php
+++ controller/deleteReportedComment.php
@@ -4,4 +4,4 @@
     $req=new CommentManager();
     $req=$req->deleteReportedComment();
 
-    echo 'Le commentaire a bien été supprimé. <br> <br> <a href="../view/backend/indexBackend.php">Retour</a>';
+    echo 'Le commentaire a bien été supprimé. <br> <br> <a href="../../index.php">Vérifier</a>';
